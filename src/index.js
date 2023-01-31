b4b01ebfa43a127e403385396b9d6d9e12cb273b
annamelya2021 <anna.basyuk@gmail.com>
2023-01-31 18:06:21 +0200

    +

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+// import 'modern-normalize';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/practice_routers">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
